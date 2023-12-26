519e095e66c080e81f85a1a42c2c7231f42ff937
CuttleFish <b9804320975@gmail.com>
2023-12-26 15:19:37 +0800

    初始化 Spring boot project

---
+++ src/main/java/com/dodo/good_luck/SpringBootMaillApplication.java
@@ -0,0 +1,13 @@
+package com.dodo.good_luck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMaillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMaillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dodo/good_luck/SpringBootMaillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dodo.good_luck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMaillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

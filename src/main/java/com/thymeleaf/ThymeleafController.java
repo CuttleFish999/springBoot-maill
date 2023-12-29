65ddb8d8e5a70911c73e27429952a75165f9c9c2
weiyu <b9804320975@gmail.com>
2023-12-29 14:10:36 +0800

    首頁連結

--- src/main/java/com/thymeleaf/ThymeleafController.java
+++ src/main/java/com/thymeleaf/ThymeleafController.java
@@ -7,40 +7,43 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.rtn.model.Rtn;
 import com.rtn.service.RtnService;
 
 @Controller
-@ComponentScan(basePackages = "com.rtn")
+@ComponentScan(basePackages = "com")
 public class ThymeleafController {
 	@Autowired
 	public RtnService rtnService;
-	
-    @GetMapping("/")
-    public String home(Model model) {
 
-    	Integer rtnCount = rtnService.getAllRtnIdCount();
-//        Rtn rtn = rtnService.getProductById(1);
-        List<Rtn> rtn = rtnService.getAllRtnData();
-        model.addAttribute("rtnCount", rtnCount);
-        model.addAttribute("rtn1",rtn);
-        return "index";
-    }
+	@GetMapping("/")
+	public String index(Model model) {
+		return "index";
+	}
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "hello";
-    }
+    @GetMapping("/Rtn")
+	public String Rtn(Model model) {
 
-    @PostMapping("/login")
-    public String login(String userName,
-                        String userPassword) {
+		Integer rtnCount = rtnService.getAllRtnIdCount();
+		List<Rtn> rtn = rtnService.getAllRtnData();
+		model.addAttribute("rtnCount", rtnCount);
+		model.addAttribute("rtn1", rtn);
+		return "Rtn";
+	}
 
-        System.out.println("userName 為: " + userName);
-        System.out.println("userPassword 為: " + userPassword);
-
-        return "login";
-    }
+//    @GetMapping("/hello")
+//    public String hello() {
+//        return "hello";
+//    }
+//
+//    @PostMapping("/login")
+//    public String login(String userName,
+//                        String userPassword) {
+//
+//        System.out.println("userName 為: " + userName);
+//        System.out.println("userPassword 為: " + userPassword);
+//
+//        return "login";
+//    }
 }

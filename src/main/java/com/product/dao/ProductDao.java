49d72ed6b5ee1d55a56e5ada37a3645efd755527
CuttleFish <b9804320975@gmail.com>
2023-12-30 18:41:53 +0800

    修正git 衝突

--- src/main/java/com/product/dao/ProductDao.java
+++ src/main/java/com/product/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.product.dao;
 
+import com.product.constant.ProductCategory;
 import com.product.dto.ProductRequest;
 import com.product.model.Product;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface ProductDao {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category , String search);
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/product/service/ProductService.java
+++ src/main/java/com/product/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.product.service;
 
+import com.product.constant.ProductCategory;
 import com.product.dto.ProductRequest;
 import com.product.model.Product;
 
@@ -7,7 +8,7 @@ import java.util.List;
 
 public interface ProductService {
 
-    List<Product> getProducts();
+    List<Product> getProducts(ProductCategory category ,String search);
 
     Product getProductById(Integer productId);
 
